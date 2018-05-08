84abd45b0b5c91f8dca3fa08faacaaca8d4d4f8a
Mátyás Budavári <budavariam@gmail.com>
2018-05-08 12:31:26 +0200

    document the folderhandler

--- src/commands/init/FolderHandler.ts
+++ src/commands/init/FolderHandler.ts
@@ -3,14 +3,29 @@ import * as path from "path";
 import { IAssetData } from "../../interfaces/IAssetData";
 import {config} from "../../utils/ProjectHandler";
 
+/**
+ * Get the asset from the assets directory.
+ *
+ * @param assetPath Asset relative to the assets directory.
+ */
 function getAsset(assetPath: string) {
     return path.resolve(__dirname, "..", config.assetsFolder, assetPath);
 }
+
+/**
+ * Build the result relative the directory that the program has been called.
+ *
+ * @param targetPath Path relative to the curret directory.
+ */
 function getTarget(targetPath: string) {
     return path.resolve(".", targetPath);
 }
 export class FolderHandler {
-    public templateFolderPath = "";
+    /**
+     * Path for the template directory in this project.
+     */
+    private templateFolderPath = "";
+
     constructor(
         assetData: IAssetData,
         public resultFolderName: string,
@@ -18,6 +33,19 @@ export class FolderHandler {
         this.templateFolderPath = getAsset(assetData.path);
         this.resultFolderName = getTarget(resultFolderName);
     }
+    /**
+     * Check if the folder exists that is about to be created.
+     *
+     * @returns Promise<boolean> true if it already exists.
+     */
+    public checkExistence(): Promise<boolean> {
+        return fs.pathExists(this.resultFolderName);
+    }
+    /**
+     * Copies the proper assets to the defined place
+     *
+     * @returns Promise<void>
+     */
     public build(): Promise<void> {
         return fs.copy(this.templateFolderPath, this.resultFolderName);
     }
