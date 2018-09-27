6ae48634943d1da630077ef31b7029907b6e789b
Javier Segura Perez <javier.segura@me.com>
2018-09-27 14:04:52 +0200

    Added iOS test app

--- CocoaLibs/Foundation/platform/web/MIOCore.ts
+++ CocoaLibs/Foundation/platform/web/MIOCore.ts
@@ -84,3 +84,14 @@ export enum MIOCoreBrowserType
 export function MIOCoreGetBrowser(): MIOCoreBrowserType {
     return MIOCoreBrowserType.Other;
 }
+
+export function NSClassFromString(className)
+{
+    let classObject = window[className];
+    if (classObject == null) classObject = MIOCoreClassByName(className);
+
+    if (classObject == null) throw new Error("MIOClassFromString: class '" + className + "' didn't register.");
+
+    let newClass = new classObject();
+    return newClass;
+}
