c0dea23ecd7269432755a3e45c62c749d3bd375d
eddyspa <edgar.pedreno@gmail.com>
2019-04-10 13:01:53 +0200

    Implemented package creation and deleting

--- Libs/Foundation/gulp.utils.js
+++ Libs/Foundation/gulp.utils.js
@@ -3,15 +3,11 @@ const fs = require("fs");
 const ts = require("gulp-typescript");
 const rollup = require('rollup');
 const rollupTypescript = require('rollup-plugin-typescript2');
+const dts = require("dts-bundle");
+const uglify = require("gulp-uglify");
+const pipeline = require('readable-stream').pipeline;
 
-const NODE_INDEX_PATH = "source/index-node.ts";
-const WEB_INDEX_PATH = "source/platform/web/index.ts";
-
-//var tsProjectProd = ts.createProject('tsconfig.json');
-var tsProjectDev = ts.createProject('tsconfig-node.json');
-
-
-function buildNodeDev() { //no sourcemaps
+function buildNodeDev() {
 	return rollup.rollup({
 		input: './source/index.ts',
 		plugins: [
@@ -19,36 +15,107 @@ function buildNodeDev() { //no sourcemaps
 		]
 	}).then(bundle => {
 		return bundle.write({
-		  file: './dist/foundation.js',
+		  file: '.build/node-dev/foundation.js',
 		  format: 'cjs',
 		  name: 'foundation',
 		  sourcemap: true
+		}).then(function(){
+			return dts.bundle({
+				name: 'foundation',
+				main: '.build/node-dev/index.d.ts'
+			});
+		});			
+	});
+}
+
+function buildWebDev() {
+	return rollup.rollup({
+		input: './source/index.ts',
+		plugins: [
+		  rollupTypescript()
+		]
+	}).then(bundle => {
+		return bundle.write({
+		  file: '.build/web-dev/foundation.js',
+		  format: 'cjs',
+		  name: 'foundation',
+		  sourcemap: true
+		}).then(function(){
+			return dts.bundle({
+				name: 'foundation',
+				main: '.build/web-dev/index.d.ts'
+			});	
+		});			
+	});
+}
+
+function buildNodeProd() {
+	return rollup.rollup({
+		input: './source/index.ts',
+		plugins: [
+		  rollupTypescript()
+		]
+	}).then(bundle => {
+		return bundle.write({
+		  file: '.build/node-prod/foundation.js',
+		  format: 'cjs',
+		  name: 'foundation',
+		  sourcemap: false
+		}).then(() => {
+			return dts.bundle({
+				name: 'foundation',
+				main: '.build/node-prod/index.d.ts'
+			});
+		}).then(() => {
+			return minifyNodeProd();
 		});
 	});
 }
 
-function buildWebDev() { //no sourcemaps
-	return gulp.src(WEB_INDEX_PATH)
-	.pipe(tsProjectDev())
-	.pipe(gulp.dest(".build/"));
+function buildWebProd() {
+	return rollup.rollup({
+		input: './source/index.ts',
+		plugins: [
+		  rollupTypescript()
+		]
+	}).then(bundle => {
+		return bundle.write({
+		  file: '.build/web-prod/foundation.js',
+		  format: 'cjs',
+		  name: 'foundation',
+		  sourcemap: false
+		}).then(() => {
+			return dts.bundle({
+				name: 'foundation',
+				main: '.build/web-prod/index.d.ts'
+			});
+		}).then(() => {
+			return minifyWebProd();			
+		});
+	});
 }
 
-function buildNodeProd() { //generate sourcemaps
-	return gulp.src(NODE_INDEX_PATH)
-	.pipe(tsProjectProd())
-	.pipe(gulp.dest(".build/"));
+function minifyNodeProd() {
+	return pipeline (
+		gulp.src(".build/node-prod/foundation.js"),
+		uglify(),
+		gulp.dest(".build/foundation-min/node-prod")
+	);
 }
 
-function buildWebProd() { //generate sourcemaps
-	return gulp.src(WEB_INDEX_PATH)
-	.pipe(tsProjectProd())
-	.pipe(gulp.dest(".build/"));
+function minifyWebProd() {
+	return pipeline (
+		gulp.src(".build/web-prod/foundation.js"),
+		uglify(),
+		gulp.dest(".build/foundation-min/web-prod")
+	);
 }
 
 module.exports = {
 	buildNodeDev,
 	buildWebDev,
 	buildNodeProd,
-	buildWebProd
-
+	buildWebProd,
+	minifyNodeProd,
+	minifyWebProd
 }
--- Libs/Foundation/gulpfile.js
+++ Libs/Foundation/gulpfile.js
@@ -2,12 +2,15 @@ const gulp = require("gulp");
 const { series, parallel } = require("gulp");
 const fs = require("fs");
 const utils = require("./gulp.utils");
+const dts = require("dts-bundle");
+const rimraf = require("rimraf");
 
 function build(cb) {
 	var env = process.env["BUILD_ENV"];
 	var platform = process.env["BUILD_PLATFORM"];
-	console.log(env);
-	console.log(platform);
+
+	console.log(env + " " + platform);
+
 	if (env == "dev" && platform == "node") {
 		utils.buildNodeDev();
 	}
@@ -23,6 +26,48 @@ function build(cb) {
 	cb();
 }
 
+function createPackage(cb) {
+	const DEST = __dirname + "/packages/mio-foundation-node/";
+	const SRC = __dirname + "/.build/node-prod/";
+
+	if(fs.existsSync(SRC)) {
+		//Create package and types folder
+		fs.mkdirSync(DEST + "types", {recursive: true});
+		
+		//Copy foundation.d.ts to types folder
+		fs.copyFileSync(SRC + "foundation.d.ts", DEST + "types/foundation.d.ts");
+
+		//Copy package.json, LICENSE AND README
+		fs.copyFileSync(__dirname + "/../../LICENSE", DEST + "LICENSE");
+		fs.copyFileSync(__dirname + "/../../README.md", DEST + "README.md");
+
+		//Copy foundation.js to the package folder
+		fs.copyFileSync(__dirname + "/.build/foundation-min/node-prod/foundation.js", DEST + "foundation.min.js");
+		console.log("Package created succesfully");
+		cleanBuild();
+	} else {
+		console.log("/.build directory does not exist");
+	}
+	cb();
+}
+
+function cleanBuild() {
+	const dir = __dirname + "/.build";
+	const rollupcache = __dirname + "/.rpt2_cache";
+
+	if(fs.existsSync(dir)) {
+		rimraf(dir, function(err) {
+			if(err) throw err;
+			rimraf.sync(rollupcache);
+			console.log("/.build folder deleted successfully");
+		});
+	} else {
+		console.log("/.build directory does not exist");
+	}
+}
+
 module.exports = {
-	build: build
+	build: build,
+	createPackage: createPackage,
+	cleanBuild: cleanBuild
 }
