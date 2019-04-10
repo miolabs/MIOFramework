89ee9ed54ca1aad3ae5929713e4ecf27017f82c3
Javier Segura <javier@miolabs.com>
2019-04-10 19:05:15 +0200

    merging

--- Libs/Foundation/gulp.utils.js
+++ Libs/Foundation/gulp.utils.js
@@ -1,30 +1,18 @@
 const gulp = require("gulp");
 const fs = require("fs");
 const ts = require("gulp-typescript");
-const rollup = require('rollup');
-const rollupTypescript = require('rollup-plugin-typescript2');
 
 const NODE_INDEX_PATH = "source/index-node.ts";
 const WEB_INDEX_PATH = "source/platform/web/index.ts";
 
-//var tsProjectProd = ts.createProject('tsconfig.json');
-var tsProjectDev = ts.createProject('tsconfig-node.json');
+var tsProjectProd = ts.createProject('tsconfig.json');
+var tsProjectDev = ts.createProject('tsconfig-node.json', { sourceMap: false });
 
 
 function buildNodeDev() { //no sourcemaps
-	return rollup.rollup({
-		input: './source/index.ts',
-		plugins: [
-		  rollupTypescript()
-		]
-	}).then(bundle => {
-		return bundle.write({
-		  file: './dist/foundation.js',
-		  format: 'cjs',
-		  name: 'foundation',
-		  sourcemap: true
-		});
-	});
+	return gulp.src("source/")
+	.pipe(tsProjectDev())
+	.pipe(gulp.dest(".build/"));
 }
 
 function buildWebDev() { //no sourcemaps
