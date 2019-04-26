158fefe9406b70bcebba99e63c43e19f5a5d07da
eddyspa <edgar.pedreno@gmail.com>
2019-04-26 17:17:58 +0200

    uikit package and gulpfile

--- Libs/UIKit/gulpfile.js
+++ Libs/UIKit/gulpfile.js
@@ -6,7 +6,12 @@ const uglify = require("gulp-uglify");
 
 let arrIndexFiles = [];
 
-function UIParseIndexWebTs(cb) {
+/********** PROD **********/
+/********** PROD **********/
+/********** PROD **********/
+
+/********** WEB **********/
+function UIParseIndexWebTs(done) {
 	var regEx = /export\s.\sfrom \'.\/(.*)\'/gm;
 	var content = fs.readFileSync("./source/index.ts", "utf8");
 	var item;
@@ -16,31 +21,44 @@ function UIParseIndexWebTs(cb) {
 	while (item = regEx.exec(content)) {		
 		arrIndexFiles.push("./source/" + item[1] + ".ts");
 	}
-	cb();
+	done();
 }
 
 function UIConcatWebTsFiles() {		
 	return gulp.src(arrIndexFiles)
 	.pipe(concat("UIKit.web.ts"))
-	.pipe(gulp.dest("./source/"))
+	.pipe(gulp.dest("./source/"));
 }
 
-function UICleanWebFoundation(cb) {
+function UICleanWebFoundation(done) {
 	const path = "./source/UIKit.web.ts";
 	var originalContent = fs.readFileSync(path, "utf8");
-	var newContent =  originalContent.replace(/import(.*)/g, "");
+	var newContent =  originalContent.replace(/import(.*)["|']\..*/g, "");
 	fs.writeFileSync(path, newContent);
-	cb();
+	done();
 }
 
-function UIMinifyWebProd(cb) {
+function UICleanWebJsFile(done) {
+	const path = "./dist/uikit.web.js";
+	const regExObject = /\}\)\((.*) = exports.*\{\}\)\);/g;
+	const ObjectReplace = "})($1 || ($1 = {}));";
+	const regExExport = /exports\..*;/g;
+	const regExObjectProperties = 'Object.defineProperty(exports, "__esModule", { value: true });';
+
+	let content = fs.readFileSync(path, "utf8");
+	let newContent = content.replace(regExObject, ObjectReplace).replace(regExExport, " ").replace(regExObjectProperties, " ");
+	
+	fs.writeFileSync(path, newContent);
+	done();
+}
+
+function UIMinifyWebProd() {
 	return gulp.src('./dist/UIKit.web.js')
 	.pipe(uglify())
 	.pipe(gulp.dest('./.build/web-prod/'));
-	cb();
 }
 
-function UICreateWebPackage(cb) {
+function UICreateWebPackage(done) {
 	const SRC = __dirname + "/.build/";
 	const DEST = __dirname + "/packages/mio-uikit-web/";
 
@@ -49,7 +67,7 @@ function UICreateWebPackage(cb) {
 		fs.mkdirSync(DEST + "types", {recursive: true});
 		
 		//Copy UIKit.d.ts to types folder
-		//fs.copyFileSync("./dist/UIKit.web.d.ts", DEST + "types/UIKit.web.d.ts");
+		fs.copyFileSync("./dist/UIKit.web.d.ts", DEST + "types/mio-uikit-web.d.ts");
 
 		//Copy UIKit js
 		fs.copyFileSync("./.build/web-prod/UIKit.web.js", DEST + "mio-uikit-web.js");
@@ -62,10 +80,10 @@ function UICreateWebPackage(cb) {
 	} else {
 		console.log("/.build directory does not exist - Execute first gulp UIminifyWebProd");
 	}
-	cb();
+	done();
 }
 
-function UIBuildWebPackageFile(cb) {
+function UIBuildWebPackageFile(done) {
 	var platform = "web";
 	var regEx = /{%platform%}/gm;
 	const DEST = "packages/mio-uikit-web/";
@@ -76,10 +94,10 @@ function UIBuildWebPackageFile(cb) {
 	var content = fs.readFileSync("packages/mio-uikit-" + platform + "/package.json", "utf8");
 	content = content.replace(regEx, platform);
 	fs.writeFileSync("packages/mio-uikit-" + platform + "/package.json", content);
-	cb();
+	done();
 }
 
-function UIRemoveTempFolders(cb) {
+function UIRemoveTempFolders(done) {
 	const buildDir = __dirname + "/.build";
 	const distDir =  __dirname + "/dist";
 
@@ -100,13 +118,24 @@ function UIRemoveTempFolders(cb) {
 	} else {
 		console.log("/dist directory does not exist");
 	}
-	cb();
+	done();
 }
 
+/********** DEV **********/
+/********** DEV **********/
+/********** DEV **********/
+
+/********** WEB **********/
+
+
 module.exports = {
-	//build: build, //Not working, fix it when implementing dev building
-	UIBuildWebProd: gulp.series(UIParseIndexWebTs, UIConcatWebTsFiles, UICleanWebFoundation),
+	//WEB PROD
+	UIBuildWeb: gulp.series(UIParseIndexWebTs, UIConcatWebTsFiles, UICleanWebFoundation),
+	UICleanWebJsFile,
 	UIMinifyWebProd,
 	UIBuildWebPackage: gulp.series(UICreateWebPackage, UIBuildWebPackageFile),
+
+	//WEB DEV
+
 	UIRemoveTempFolders
 }
