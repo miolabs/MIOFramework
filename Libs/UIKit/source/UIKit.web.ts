8bb1be623866fe04a91641431cc243f46f85cf8c
eddyspa <edgar.pedreno@gmail.com>
2019-05-29 18:47:53 +0200

    merge

--- Libs/UIKit/source/UIKit.web.ts
+++ Libs/UIKit/source/UIKit.web.ts
@@ -3927,8 +3927,19 @@ export class UITableViewController extends UIViewController
 {
     tableView:UITableView = null;
 
+    viewDidLoad(){
+      super.viewDidLoad();
+
+      let temp = this.view.subviews[0] as UITableView;
+      this.tableView = temp
+      this.tableView.dataSource = this;
+      this.tableView.delegate = this;
+    }
+
     viewWillAppear(animated?:boolean){
       super.viewWillAppear(animated);
+
+      this.tableView.reloadData();
     }
 }
 
