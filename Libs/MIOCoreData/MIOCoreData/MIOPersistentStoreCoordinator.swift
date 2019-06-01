8f20aeec78ead9a333f4ea98948a55ce2c446a44
Javier Segura <javier@miolabs.com>
2019-06-01 16:25:55 -0700

    Working on core data stuff

---
+++ Libs/MIOCoreData/MIOCoreData/MIOPersistentStoreCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  MIOPersistentStoreCoordinator.swift
+//  MIOCoreData
+//
+//  Created by Javier Segura Perez on 31/05/2019.
+//  Copyright © 2019 Javier Segura Perez. All rights reserved.
+//
+
+import Foundation
