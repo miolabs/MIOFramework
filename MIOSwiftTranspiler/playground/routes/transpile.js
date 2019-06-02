9475680040a18c6e81e5ca0a032e8dfbb9c896c2
Marcel Ganczak <marcelganczak@gmail.com>
2019-06-02 14:33:31 +0200

    better example in web preview for transpiler

--- MIOSwiftTranspiler/playground/routes/transpile.js
+++ MIOSwiftTranspiler/playground/routes/transpile.js
@@ -53,7 +53,7 @@ router.post('*', function(req, res, next) {
         err2 => {
             console.log('!!!!!!!!!!err');
             console.log(err);
-            if(err) return res.status(500).send(err.message || err);
+            if(!transpiled) return res.status(500).send((err && err.message) || err);
             res.send(transpiled);
         })
     });
