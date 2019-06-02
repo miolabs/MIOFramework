f2c19e001acd60daeae78b0ef00c0e09317b9d29
Marcel Ganczak <marcelganczak@gmail.com>
2019-06-02 11:41:43 +0200

    web preview for transpiler

---
+++ MIOSwiftTranspiler/playground/www.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+const http = require('http');
+const port = 8080;
+
+app.set('port', port);
+
+var server = http.createServer(app);
+server.listen(port);
+server.on('listening', function(){ console.log('Listening on ' + port) });
